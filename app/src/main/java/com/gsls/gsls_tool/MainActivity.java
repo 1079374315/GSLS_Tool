5786606ac43fb493240a2373b355299283910d45
1079374315 <1079374315@qq.com>
2022-09-19 12:20:31 +0800

    Initial commit

---
+++ app/src/main/java/com/gsls/gsls_tool/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gsls.gsls_tool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ gt/src/main/java/com/gsls/gt/GT.java
@@ -0,0 +1,11 @@
+package com.gsls.gt;
+
+import android.util.Log;
+
+public class GT {
+
+    public static void log2(String msg){
+        Log.i("GT_i", msg);
+    }
+
+}
