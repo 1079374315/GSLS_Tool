7e6879222ff3d2b4480804ed41d2a09bc5a986b1
1079374315 <1079374315@qq.com>
2024-05-07 23:18:07 +0800

    * <p>  * <p>  * 更新时间:2024.5.7  * 更新内容 v1.4.5.6 版本：  * CSDN 博客/官网教程:https://blog.csdn.net/qq_39799899  * GitHub https://github.com/1079374315/GT  * 更新内容如下：  * 1.优化 WebView SslErrorHandler 事件，上架googleplay 遇见的问题  * 2.优化 Android混合开发中 WebView 上传图片的问题，并增加优化了  拍照、相册选着图片、相册选择视频、拍摄视频、本地文件上传 功能。  * 3.更新了 GT动画封装库的次数逻辑  * 4.增加 路由框架 GT.ARouter，教程请参考：https://blog.csdn.net/qq_39799899

--- app/src/main/java/com/gsls/gsls_tool/MainActivity.java
+++ app/src/main/java/com/gsls/gsls_tool/MainActivity.java
@@ -1,10 +1,8 @@
 package com.gsls.gsls_tool;
 
 import android.os.Bundle;
-import android.view.View;
 
 import com.gsls.gt.GT;
-import com.gsls.gt_databinding.annotation.GT_DataBinding;
 
 @GT.Annotations.GT_AnnotationActivity(R.layout.activity_main)
 public class MainActivity extends GT.GT_Activity.AnnotationActivity {
